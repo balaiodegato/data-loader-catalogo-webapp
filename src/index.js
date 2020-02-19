2753364625cd3626827ed45a3f6a1818e19f7576
danieltorres31 <danieltorres31@outlook.com>
2020-02-18 21:54:57 -0300

    Metodo buscar todos animais

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const converter = require('csvtojson')
 const db = require('./config/firebaseConfig').firestore()
+const animals = require('../tests/animals.json')
 
 const SHEET_PATH = 'C:\\Users\\Danie\\Desktop\\Gatos.csv'
 
 async function main() {
     try {
-        const animals = await convertCsvToJSON(SHEET_PATH)
+        // const animals = await convertCsvToJSON(SHEET_PATH)
 
-        await insertAnimal(animals[0])
+        getAnimals()
+
+        // await insertAnimal(animals[0])
         // animals.map(async animal => {
         //     await insertAnimal(animal)
         // })
@@ -28,6 +31,19 @@ function insertAnimal(animal) {
     } catch (err) {
         throw err.message
     }
+}
+
+async function getAnimals() {
+    const animalsRef = db.collection('animals');
+    animalsRef.get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                console.log(doc.id, '=>', doc.data());
+            });
+        })
+        .catch(err => {
+            console.log('Error getting documents', err);
+        });
 }
 
 function clearAllAnimals() {
